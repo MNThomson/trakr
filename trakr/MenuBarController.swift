38b9ace3109f90764fc01a19ece35214563dabf0
Max Thomson <git@mnt.dev>
2026-01-03 14:56:12 -0800

    feat: add menu bar status item with quit option

--- trakr/MenuBarController.swift
+++ trakr/MenuBarController.swift
@@ -1,5 +1,39 @@
 import AppKit
 
 class MenuBarController {
-    init() {}
+
+    private var statusItem: NSStatusItem
+    private var menu: NSMenu
+
+    init() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        menu = NSMenu()
+
+        setupStatusButton()
+        setupMenu()
+    }
+
+    private func setupStatusButton() {
+        guard let button = statusItem.button else { return }
+        button.image = NSImage(
+            systemSymbolName: "clock",
+            accessibilityDescription: "Activity Timer"
+        )
+        button.image?.isTemplate = true
+    }
+
+    private func setupMenu() {
+        menu.addItem(createMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q"))
+        statusItem.menu = menu
+    }
+
+    private func createMenuItem(title: String, action: Selector, keyEquivalent: String = "") -> NSMenuItem {
+        let item = NSMenuItem(title: title, action: action, keyEquivalent: keyEquivalent)
+        item.target = self
+        return item
+    }
+
+    @objc private func quitApp() {
+        NSApplication.shared.terminate(nil)
+    }
 }
--- trakr/TrakrApp.swift
+++ trakr/TrakrApp.swift
@@ -1,10 +1,21 @@
 import SwiftUI
+import AppKit
 
 @main
 struct TrakrApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         Settings {
             EmptyView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    private var menuBarController: MenuBarController?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        menuBarController = MenuBarController()
     }
 }
